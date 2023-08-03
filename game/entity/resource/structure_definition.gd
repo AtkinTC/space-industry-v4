db518c6078e4f6b180778205e36a17c9733935eb
AtkinTC <Atkintc@gmail.com>
2023-08-02 23:19:39 -0400

    add StructureGridTool to set the grid area of a structure, accessable from StructureDefinition resource

--- game/entity/resource/structure_definition.gd
+++ game/entity/resource/structure_definition.gd
@@ -2,3 +2,29 @@ extends EntityDefinition
 class_name StructureDefinition
 
 @export var dock_range : float = 50
+
+const GRID_TOOL_NODE_NAME := "StructureGridTool"
+const GRID_TOOL_GRID_SIZE_PROP_NAME := "grid_size"
+var grid_size_checked := false
+var grid_size_found := false
+var grid_size :=  Vector2i(1, 1) : get = get_grid_size
+
+func get_grid_size() -> Vector2i:
+	if(grid_size_checked == true):
+		return grid_size
+	
+	var scene_state := scene.get_state()
+	for i in scene_state.get_node_count():
+		if(scene_state.get_node_name(i) == GRID_TOOL_NODE_NAME):
+			for j in scene_state.get_node_property_count(i):
+				if(scene_state.get_node_property_name(i, j) == GRID_TOOL_GRID_SIZE_PROP_NAME):
+					grid_size = scene_state.get_node_property_value(i, j)
+					grid_size_found = true
+					break
+			break
+	
+	if(!grid_size_found):
+		print_debug("No grid_size property in scene, using default unit grid size.")
+	
+	grid_size_checked = true
+	return grid_size
---
+++ game/entity/structure/structure_grid_tool.gd
@@ -0,0 +1,29 @@
+@tool
+extends Node2D
+class_name StructureGridTool
+
+@export var grid_size := Vector2i(1,1) : set = set_grid_size
+
+var top_left : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		queue_free()
+		return
+	recalculate()
+
+func set_grid_size(size : Vector2i) -> void:
+	if(size.x <= 0 || size.y <= 0):
+		return
+	grid_size = size
+	recalculate()
+
+func recalculate() -> void:
+	top_left = -grid_size * Constants.TILE_SIZE / 2.0
+	queue_redraw()
+
+func _draw() -> void:
+	draw_rect(Rect2(-grid_size * Constants.TILE_SIZE / 2.0, grid_size * Constants.TILE_SIZE), Color.RED, false)
+	draw_circle(top_left, 3, Color.RED)
+	draw_circle(top_left + Constants.TILE_SIZE / 2.0, 3, Color.BLUE)
